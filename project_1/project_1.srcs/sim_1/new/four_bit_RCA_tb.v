7b814cb9ce3208871ed9cf304eaba3e0bf93b84c
Devarsh Gandhi <140993391+devy03@users.noreply.github.com>
2025-02-27 12:01:17 -0600

    Added 4-bit RCA.v code and basic testbench
    needs revision on testbench code for part (d) and (e)

---
+++ project_1/project_1.srcs/sim_1/new/four_bit_RCA_tb.v
@@ -0,0 +1,62 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/27/2025 11:30:50 AM
+// Design Name: 
+// Module Name: four_bit_RCA_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module four_bit_RCA_tb;
+    
+    reg [3:0] A, B;  
+    reg C_in;        
+    wire [3:0] S;    
+    wire C_out;      
+
+    
+    four_bit_RCA uut (
+        .A(A),
+        .B(B),
+        .C_in(C_in),
+        .S(S),
+        .C_out(C_out)
+    );
+
+ 
+    initial begin
+        $monitor("Time=%0t | A=%b B=%b C_in=%b | S=%b C_out=%b", $time, A, B, C_in, S, C_out);
+        
+        // 3 + 5 = 8
+        A = 4'b0011; B = 4'b0101; C_in = 0;
+        #10;
+        
+        // 7 + 1 = 8
+        A = 4'b0111; B = 4'b0001; C_in = 0;
+        #10;
+
+        //5 + 10 = 15
+        A = 4'b0101; B = 4'b1010; C_in = 0;
+        #10;
+        
+        //(overflow case)
+        A = 4'b1111; B = 4'b0001; C_in = 0;
+        #10;
+        
+
+        $stop;  
+    end
+endmodule
--- project_1/project_1.srcs/sources_1/new/four_bit_RCA.v
+++ project_1/project_1.srcs/sources_1/new/four_bit_RCA.v
@@ -27,4 +27,12 @@ module four_bit_RCA(
     output [3:0] S,
     output C_out
     );
+    //updated code below
+     wire C1, C2, C3;  //wires
+
+    // Instantiate the 1-bit adders
+    fulladder_structural FA0 (A[0], B[0], C_in, S[0], C1);//C_out ---> C1
+    fulladder_structural FA1 (A[1], B[1], C1,   S[1], C2); //C1 ---> C_in(1)
+    fulladder_structural FA2 (A[2], B[2], C2,   S[2], C3);//C2 ---> C_in(2)
+    fulladder_structural FA3 (A[3], B[3], C3,   S[3], C_out);//C3 ---> C_in(3)----> C_out
 endmodule
