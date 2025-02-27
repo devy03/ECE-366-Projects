1d6c9003f9932c660d55a8572fb242e71fefb200
Devarsh Gandhi <140993391+devy03@users.noreply.github.com>
2025-02-27 11:26:47 -0600

    upto 1(c)
    ) - added four_bit_RCA.v

---
+++ project_1/project_1.srcs/sources_1/new/four_bit_RCA.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/27/2025 11:21:29 AM
+// Design Name: 
+// Module Name: four_bit_RCA
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module four_bit_RCA(
+    input [3:0] A,
+    input [3:0] B,
+    input C_in,
+    output [3:0] S,
+    output C_out
+    );
+endmodule
